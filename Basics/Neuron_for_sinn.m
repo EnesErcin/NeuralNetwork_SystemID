7588b3d3a1eee23a24919a16b04c7678343f239f
EnesErcin <ercine20@itu.edu.tr>
2023-03-30 18:35:37 +0300

    Run the model, reasonable results so far need further evaluation

--- Basics/Neuron_for_sinn.m
+++ Basics/Neuron_for_sinn.m
@@ -1,4 +1,4 @@
-classdef Neuron_for_sinn
+classdef Neuron_for_sinn <handle
    properties
     input_num               ;
     mu              = 0.001 ;   %Learning Rate
@@ -8,41 +8,71 @@ classdef Neuron_for_sinn
     a_cache                 ;
     y_cache                 ;
     d_cache                 ;
+    count                   ;
+    input_cache             ;
+    % Model Evaluation
+    weigth_results          ;
+    cost_result             ;
+    test_num                ;
    end
    methods
        
-      function obj = Neuron_for_sinn(num)
+      function obj = Neuron_for_sinn(num,test_num)
             % Constructor
             obj.input_num = num;
             %   Initate weigth and bias
             obj.w  = 0.001*randn([num,1]); %Weigths Initited close to 
-            obj.b  = 0.001*randn;
+            obj.b  = 0.000001*randn;
             
             obj.a_cache                 =zeros([num,1]);
             obj.y_cache                 =zeros([num,1]);
             obj.d_cache                 =0;
+            obj.count                   =0;
+            obj.input_cache             =zeros([num,1]);
+            obj.cost_result             =zeros([test_num,1]);
+            obj.test_num                =test_num; 
+            obj.weigth_results          =zeros([test_num,num]);
       end
       
-      function [obj] = feedforward(obj,x,exp)
+      function [obj,exp] = feedforward(obj,x,exp,train)
+          
             assert(length(x)==obj.input_num,"Input number should be equal to input_num %d",obj.input_num);
-            obj.a_cache =   dot(obj.w,x) + obj.b; %Linear Layer
-            obj.y_cache =   sigmoid(obj.a_cache);
+            
+            obj.input_cache = x;
+            obj.a_cache =   obj.w.*x + obj.b; %Linear Layer
+            for i = 1:obj.input_num
+                obj.y_cache(i) =   sigmoid(obj.a_cache(i));
+            end
             obj.d_cache =   exp;
             temp = 0;
             for i = 1:obj.input_num
                 temp = temp + (exp-obj.y_cache(i))^2;
             end
+            
             obj.loss =     temp;
+            if train == 1 
+                
+                obj.count =   obj.count + 1;
+                obj.cost_result(obj.count) = temp;
+                obj.weigth_results(obj.count,:)  = obj.w;
+            end
       end
       function obj = backprop(obj)
-          dC = -2*(obj.d_cache-obj.y_cache); %Cost Function -> dC/dY
-          dy = obj.y_cache*(1-obj.y_cache);  %Y -> dY/da
-          da = obj.a_cache;                  %a -> da/dw
-          dw = da*dy*dC;                     %dw -> dC/dw -> dC/dY*dY/da*da/dw
-            
-          db = da*dy;                        %dw -> dC/db -> dC/dY*dY/da*da/db
+          dC_dY = zeros(obj.input_num,1);
+ 
+          for i = 1:obj.input_num
+            dC_dY(i) = -2*(obj.d_cache-obj.y_cache(i)); %Cost Function -> dC/dY
+          end
+         
+    
+          dY_da = obj.y_cache.*(ones(obj.input_num,1)-obj.y_cache);  %Y -> dY/da
+         
+          da_dw = obj.input_cache;           %a -> da/dw
+          dw = dC_dY.*dY_da.*da_dw;          %dw -> dC/dw -> dC/dY*dY/da*da/dw
+          db = dC_dY.*dY_da;                 %dw -> dC/db -> dC/dY*dY/da*da/db
           obj.w = obj.w -obj.mu*dw;
           obj.b = obj.b -obj.mu*db ;
+         
       end
    end
 end 
--- Basics/Run.m
+++ Basics/Run.m
@@ -2,19 +2,48 @@ clc; clear; close all;
 
 
 input_ports     = 5;
-test_size       = 250;
+test_size       = 2500;
 type_of_data    ="sin";
 sample_freq     = 100;
 
 [fulldata,preped] =data_prep(type_of_data,input_ports,sample_freq,test_size);
 
 expected_values = preped(1,2:end);
-
-
-My_model = Neuron_for_sinn(input_ports);
+My_model = Neuron_for_sinn(input_ports,test_size);
 
 for i = 1:test_size-1
-    My_model.feedforward(preped(:,i),expected_values(:,i));
+    My_model.feedforward(preped(:,i),expected_values(:,i),1);
     My_model.backprop();
+    disp(My_model.count);
 end
+
+validate_size = 250;
+results = zeros([validate_size,1]);
+compare = zeros([validate_size,1]);
+
+for i = 1:validate_size
+    My_model.feedforward(preped(:,i),expected_values(:,i),0);
+    results(i)= sum(My_model.y_cache);
+end
+for i = 1:validate_size
+    compare(i) = expected_values(:,i);
+end
+what = results-compare;
+
+
+subplot(3,1,1);
+title("Real");
+plot(compare)
+
+subplot(3,1,2); 
+title("Estimated");
+plot(results)
+
+subplot(3,1,3); 
+plot(results)
+hold on
+plot(compare)
+hold off
+
+
 
