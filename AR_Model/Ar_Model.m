a9694fd12ccffe513ef9cf3c1b00ef1625aed329
EnesErcin <ercine20@itu.edu.tr>
2023-04-17 09:09:50 +0300

    Fixed the model prediction

--- AR_Model/Ar_Model.m
+++ AR_Model/Ar_Model.m
@@ -6,35 +6,44 @@ figure;
 plot(t(1:100),x(1:100),'b-',t(1:100),x(1:100)-x_n(1:100),'r--');
 legend('Signal', 'Noise');
 title("Noisy and Original Signal");
+noise = x_n -x;
 
 deg = 8;
-sys  = ar(x_n,deg);
-temp = zeros(deg+1,length(x_n));
+sys  = ar(x_n,deg-1);
+temp = zeros(deg,length(x_n));
 
 for i = 1:length(x_n)-deg
-        temp(1,i) = 1;
-    for j = 2:deg+1
-        temp(j,i) = x_n(i+j-1);
+    for j = 1:deg
+        temp(j,i) = x_n(i+j);
     end
 end
 
 res = zeros(1,length(x_n));
+
+test = (temp(:,4).*sys.a');
 for i = 1:length(x_n)-deg
     res(i) = sum(temp(:,i).*sys.a');
 end
 
 figure;
-plot(t(:,(1:end-4)), x_n(:,(1:end-4)), 'b-', t(:,(1:end-4)), 1-res(:,(1:end-4)), 'r-');
-legend('Signal with noise', 'AR model output');
+subplot(2,1,1);
+plot(t(:,(1:end-4)), x(:,(1:end-4)), 'b-', t(:,(1:end-4)), x_n(:,(1:end-4))-res(:,(1:end-4)),'r-', t(:,(1:end-4)),x_n(:,(1:end-4)),"g-.");
+legend('Signal without noise', 'AR model output pred', "Signal With Noise");
 xlabel('Time (s)');
 ylabel('Amplitude');
-title("AR model of order" + deg);
 
-data = iddata((x_n)',[]);
-fd = iddata((x_n)',[]);
-dtp =100;
-yf = forecast(sys,data(1:100),dtp);
-figure;
-plot(data(1:100),'b',yf,'r'), legend('measured','forecasted');
-title("AR model forecasted "+dtp  +" data point ahead");
+subplot(2,1,2);
+plot(t(:,(1:end-4)), noise(:,(1:end-4)), 'b-', t(:,(1:end-4)), res(:,(1:end-4)), 'r-');
+legend('Signal noise', 'AR model estimation');
+xlabel('Time (s)');
+ylabel('Amplitude');
+
+
+error = abs(x(:,(1:end-4))-x_n(:,(1:end-4)));
+error = mean(error);
+estimated_error = x(:,(1:end-4))-res(:,(1:end-4));
+estimated_error = mean(estimated_error);
+fprintf("Reduced average error by Ar model : %d, Initial Error: %d \n",estimated_error,error);
+fprintf("System accuracy has improved by %.2f%% \n" , 100*(error-estimated_error)/error);
+
 
